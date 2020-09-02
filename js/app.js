70ab0be2c9d90f5c03a597844581dd3153770681
brendon cecilio <brendon.cecilio@gmail.com>
2020-09-01 21:38:51 -0400

    added skycons.js

--- js/app.js
+++ js/app.js
@@ -19,11 +19,20 @@ window.addEventListener("load", () => {
 			})
 			.then(data => {
 				console.log(data);
-				const {temperature, summary} = data.currently;
+				const {temperature, summary, icon} = data.currently;
 				// set DOM elements from the API
 				temperatureDegree.textContent = temperature;
 				temperatureDescription.textContent = summary;
+				locationTimezone.textContent = data.timezone;
+				setIcons(icon, document.querySelector('.icon'))
 			})
 		});
 	}
+
+	function setIcons(icon, iconID) {
+		const skycons = new Skycons({color: "white"});
+		const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+		skycons.play();
+		return skycons.set(iconID, Skycons[currentIcon])
+	} 
 });
